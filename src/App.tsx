9126d09b920259f900354e6d839027c2f8ab5626
WDever <alsrbtkfkd7@gmail.com>
2019-03-02 22:45:48 +0900

    Prepare to move to TS
    Copy src dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import * as React from 'react';
+import React from 'react';
+// import { Redirect } from 'react-router-dom';
+import MusicListContainer from './container/MusicListContainer';
+import PageTemplate from './components/PageTemplate';
+import SearchBarContainer from './container/SearchBarContainer';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <PageTemplate
+        list={<MusicListContainer />}
+        search={<SearchBarContainer />}
+      />
     );
   }
 }
